2aace4999830fc936940654aab1ba0d7414fb6a3
Nikita <eternalxbless@gmail.com>
2022-04-19 08:15:27 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+
+const obj = {
+    name: "John",
+    age: 25,
+    isMarried: true
+};
+
+// console.log(obj.name)
+
+console.log(obj["name"]);
+
+let arr = ['plum.png', 'orange.jpeg', 'apple.bmp', 6, {}, []];
+
+console.log(arr[1])
+
+let zopa = 10,
+    pizda = 10;
+
+zopa++;
+pizda--;
+console.log(zopa,pizda);
