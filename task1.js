f348172edcc267e0e609ffbf1331991c7908633c
Nikita <eternalxbless@gmail.com>
2022-04-19 18:22:51 +0300

    Second task
    I made a second task from first practic lesson

--- task1.js
+++ task1.js
@@ -1,4 +1,11 @@
 "use strict";
 
 const numberOfFilms = prompt('Сколько фильмов вы уже посмотрели?', "");
-console.log(numberOfFilms);
+
+let personalMovieDB = {
+    count: numberOfFilms,
+    movies: '',
+    actors: '',
+    genres: [],
+    privat: false
+};
